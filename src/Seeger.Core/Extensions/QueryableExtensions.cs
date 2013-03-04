3eb064981c07cc1f1e1ee89bcaffce50f1c549b0
mouhong <dylan.lin@live.com>
2013-03-04 14:10:28 +0800

    Remove duplicate Paging extension method; add publish profile for ImageSlider plugin

--- src/Seeger.Core/Extensions/QueryableExtensions.cs
+++ src/Seeger.Core/Extensions/QueryableExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Seeger.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,8 +8,17 @@ namespace Seeger
 {
     public static class QueryableExtensions
     {
+        public static IQueryable<T> FilterWith<T>(this IQueryable<T> query, IQueryFilter<T> filter)
+        {
+            Require.NotNull(query, "query");
+            Require.NotNull(filter, "filter");
+
+            return filter.ApplyTo(query);
+        }
+
         public static PagedQueryable<T> Paging<T>(this IQueryable<T> query, int pageSize)
         {
+            Require.NotNull(query, "query");
             return new PagedQueryable<T>(query, pageSize);
         }
     }
